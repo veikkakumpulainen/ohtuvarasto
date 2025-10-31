fae0045d203ba0feda7d0683a38ecd4db5304ca9
Veikka Kumpulainen <143580336+veikkakumpulainen@users.noreply.github.com>
2025-10-31 13:02:39 +0200

    muutos kommentissa

--- src/varasto.py
+++ src/varasto.py
@@ -13,7 +13,7 @@ class Varasto:
             # mahtuu
             self.saldo = alku_saldo
         else:
-            # täyteen ja ylimäärä hukkaan!
+            # täyteen ja ylimäärä hukkaan!!
             self.saldo = tilavuus
 
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
